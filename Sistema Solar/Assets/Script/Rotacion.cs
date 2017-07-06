4380ca1f7e73f04e69e64ea0ff78b04e8c0ce823
juanmanuel102341 <jms24@live.com.ar>
2017-07-06 09:52:58 -0300

    committie proyecto base(viene d rep desktop), sistema solar, agregue lensflare, y mejore un poco la luz y el orden interno carpetas

---
+++ Sistema Solar/Assets/Script/Rotacion.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotacion : MonoBehaviour {
+	
+	public  float velocity_rotacion;
+	void Update () {
+		
+		transform.Rotate(0,velocity_rotacion,0);
+
+	}
+}
---
+++ Sistema Solar/Assets/Script/Traslacion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traslacion : MonoBehaviour {
+
+
+	public Transform target;
+	public float angel;
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(target.position,Vector3.up,angel);
+
+	}
+}
